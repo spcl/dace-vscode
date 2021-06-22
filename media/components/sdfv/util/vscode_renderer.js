e2f512d9d2f2ebabbd32d00f0e1059275547a304
Philipp Schaad <schaad.phil@gmail.com>
2021-06-22 16:09:57 +0200

    Fix conflicts from merging

--- media/components/sdfv/util/vscode_renderer.js
+++ media/components/sdfv/util/vscode_renderer.js
@@ -6,12 +6,34 @@ class VSCodeRenderer extends daceSDFGRenderer {
     bpIndicator;
 
     constructor(sdfg, container, on_mouse_event = null, user_transform = null,
-        debug_draw = false, background = null) {
+        debug_draw = false, background = null, mode_buttons = null) {
 
         dom_setup();
 
+        if (!mode_buttons) {
+            let pan_btn = document.getElementById('pan-btn');
+            let move_btn = document.getElementById('move-btn');
+            let select_btn = document.getElementById('select-btn');
+            let add_btns = [
+                document.getElementById('elem_map'),
+                document.getElementById('elem_consume'),
+                document.getElementById('elem_tasklet'),
+                document.getElementById('elem_nested_sdfg'),
+                document.getElementById('elem_access_node'),
+                document.getElementById('elem_stream'),
+                document.getElementById('elem_state'),
+            ];
+            if (pan_btn)
+                mode_buttons = {
+                    pan: pan_btn,
+                    move: move_btn,
+                    select: select_btn,
+                    add_btns: add_btns
+                };
+        }
+
         super(sdfg, container, on_mouse_event, user_transform,
-            debug_draw, background);
+            debug_draw, background, mode_buttons);
 
         this.bpIndicator = new BreakpointIndicator(this);
     }
@@ -25,6 +47,10 @@ class VSCodeRenderer extends daceSDFGRenderer {
         super.on_mouse_event(event, comp_x_func, comp_y_func, evtype);
         this.bpIndicator.handle_mouse_event(event, comp_x_func,
             comp_y_func, evtype);
+    }
+
+    send_new_sdfg_to_vscode() {
+        vscode_write_graph(this.sdfg);
     }
 
 }
@@ -51,5 +77,4 @@ function dom_setup(){
             e.preventDefault();
         }
     });
-
 }
